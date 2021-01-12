9756ba3a28335013ea3e0791bf8212bf5b5c2359
Matthew-Bjorkman <matthewebjorkman@gmail.com>
2021-01-11 18:51:44 -0600

    Initial Files
    Initial meta files for 1st chapter of "Head First Design Patterns"

---
+++ DesignPatterns/Ch1_StrategyPattern/MainFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ch1_StrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
